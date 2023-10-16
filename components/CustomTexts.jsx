ce182407bdee52081f40ac37f13a7ad817328fc1
abdulladone94 <abdullaf@booleanlabs.biz>
2023-10-16 12:31:12 +0530

    first commit

---
+++ components/CustomTexts.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { textContainer, textVariant2 } from "../utils/motion";
+
+export const TypingText = ({ title, textStyles }) => (
+  <motion.p
+    variants={textContainer}
+    className={`${textStyles} font-normal text-[14px] text-secondary-white`}
+  >
+    {Array.from(title).map((Letter, index) => (
+      <motion.span className={textVariant2} key={index}>
+        {Letter === " " ? "\u00A0" : Letter}
+      </motion.span>
+    ))}
+  </motion.p>
+);
+
+export const TitleText = ({ title, textStyles }) => (
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`${textStyles} mt-[8px] font-bold md:text-[64px] text-[40px] text-white`}
+  >
+    {title}
+  </motion.h2>
+);
